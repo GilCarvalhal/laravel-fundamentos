a5e61595cdb6b9b4fbcb3db3708178c7ad30d3ef
Gil Carvalhal <gilbertocarvalhalmatos@gmail.com>
2023-10-26 20:57:30 -0300

    avancando na template engine blade (loop)

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -846,6 +846,25 @@
     @else
     <p>O nome não é Gilberto</p>
     @endif
+
+    @for($i = 0; $i < count($arr); $i++) <p>{{$arr[$i]}} - {{$i}}</p>
+        @if($i == 2)
+        <p>O índice é 2</p>
+        @endif
+        @endfor
+
+        @foreach($nomes as $nome)
+        <p>{{$loop->index}}</p>
+        <p>{{$nome}}</p>
+        @endforeach
+
+        @php
+        $name = "Gil";
+        echo "Meu nome é " . $name;
+        @endphp
+
+        <!-- Comentário do HTML aparece no inspecionar -->
+        {{-- Já o comentário do Blade não aparece --}}
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,10 @@ Route::get('/', function () {
     $nome = "Gil";
     $idade = 32;
 
-    return view('welcome', ["nome" => $nome, "idade" => $idade, "profissão" => "programador"]);
+    $arr = [10, 20, 30, 40, 50];
+    $nomes = ["Gil", "Gilberto", "Gabriel"];
+
+    return view('welcome', ["nome" => $nome, "idade" => $idade, "profissão" => "programador", "arr" => $arr, "nomes" => $nomes]);
 });
 
 Route::get('/contact', function () {
