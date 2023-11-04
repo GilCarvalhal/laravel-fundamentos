4e01d55d507814b0d056609ee85e5a62e6866704
Gil Carvalhal <gilbertocarvalhalmatos@gmail.com>
2023-11-03 23:16:52 -0300

    adicionando arquivos estaticos

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,2 +1,19 @@
-<h1>Tela de produtos</h1>
-<a href="/">Voltar para o home</a>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <link rel="stylesheet" href="/css/styles.css">
+    <script src="/js/scripts.js"></script>
+</head>
+
+<body>
+    <h1>Tela de produtos</h1>
+    <a href="/">Voltar para o home</a>
+</body>
+
+</html>
